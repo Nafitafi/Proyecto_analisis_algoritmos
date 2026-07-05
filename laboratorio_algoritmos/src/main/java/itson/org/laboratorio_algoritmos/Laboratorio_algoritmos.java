9c262830c99d3840800d70e4a134dde304efe0da
Nafitafi <nafibriones@gmail.com>
2026-07-04 23:43:16 -0700

    build: Creacion del proyecto

---
+++ laboratorio_algoritmos/src/main/java/itson/org/laboratorio_algoritmos/Laboratorio_algoritmos.java
@@ -0,0 +1,12 @@
+package itson.org.laboratorio_algoritmos;
+
+/**
+ *
+ * @author nafbr
+ */
+public class Laboratorio_algoritmos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
